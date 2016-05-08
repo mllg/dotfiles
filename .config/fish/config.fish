9446aec55a4ae206f4edb7ffcc1db46552859f27
Michel Lang <michellang@gmail.com>
2016-05-08 16:56:40 +0200

    fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,8 @@ abbr -a gs git status
 abbr -a gst git status
 abbr -a gp git push
 abbr -a gco git checkout
+abbr -a gl git pull
+
 alias ll="ls -lhF --color=auto --group-directories-first --time-style=+%Y-%m-%d\ %H:%M"
 alias la="ls -lhaF --color=auto --group-directories-first --time-style=+%Y-%m-%d\ %H:%M"
 alias du="du -h"
@@ -22,7 +24,13 @@ alias depclean="sudo pacman -Rns (pacman -Qtdq)"
 
 function my_vi_key_bindings
     fish_vi_key_bindings
+    bind -M insert \ct '__fzf_ctrl_t'
+    bind -M insert \cr '__fzf_ctrl_r'
+    bind -M insert \cx '__fzf_ctrl_x'
+    bind -M \insert \ec '__fzf_alt_c'
     bind -M insert \cf accept-autosuggestion
     bind \cf accept-autosuggestion
+    bind \cr accept-autosuggestion
 end
+
 set -g fish_key_bindings my_vi_key_bindings
