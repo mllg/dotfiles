43addfad09f735becc892d640d32cb0839db1990
Michel Lang <michellang@gmail.com>
2016-05-20 23:16:24 +0200

    added homebrew env var

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -51,6 +51,7 @@ set -gx COLORTERM 1
 function set_secrets
     set -xU OPENMLAPIKEY (pass show openml.api.key)
     set -xU GITHUB_TOKEN (pass show github.token)
+    set -xU HOMEBREW_GITHUB_API_TOKEN (pass show github.token)
 end
 
 function update
