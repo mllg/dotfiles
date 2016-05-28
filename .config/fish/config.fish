1dddcf58b02b5d64270033972790b493417bd640
Michel Lang <michellang@gmail.com>
2016-05-28 16:08:11 +0200

    update to fish 2.3

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -24,19 +24,7 @@ alias depclean="sudo pacman -Rns (pacman -Qtdq)"
 alias ...="cd ../../"
 alias ....="cd ../../../"
 
-function my_vi_key_bindings
-    # Workaround for https://github.com/fish-shell/fish-shell/issues/2254
-    fish_vi_key_bindings
-    bind -M insert \ct '__fzf_ctrl_t'
-    bind -M insert \cr '__fzf_ctrl_r'
-    bind -M insert \cx '__fzf_ctrl_x'
-    bind -M \insert \ec '__fzf_alt_c'
-    bind -M insert \cf accept-autosuggestion
-    bind \cf accept-autosuggestion
-    bind \cr accept-autosuggestion
-end
-set -g fish_key_bindings my_vi_key_bindings
-
+fish_vi_key_bindings
 set -gx EDITOR nvim
 set -gx SUDO_EDITOR nvim
 set -gx MANPAGER "/bin/sh -c 'col -b | nvim -c MANPAGER -'"
@@ -95,10 +83,6 @@ function update
         info "Updating R packages"
         rupdate
     end
-end
-
-for file in ~/.config/fish/conf.d/*.fish
-    source $file
 end
 
 if test -r ~/.config/fish/local.fish
