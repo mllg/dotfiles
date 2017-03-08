fc6cfe70df47702ea6f94d3d8c8510c03ff98176
Michel Lang <michellang@gmail.com>
2017-03-08 10:52:57 +0100

    fzf

--- .config/fish/functions/__fzf_ctrl_o.fish
+++ .config/fish/functions/__fzf_ctrl_o.fish
@@ -1,6 +1,6 @@
 function __fzf_ctrl_o
     set -l FZF_CTRL_O_COMMAND "git ls-files (git rev-parse --show-toplevel)"
-    fish -c "$FZF_CTRL_O_COMMAND" | __fzfcmd -m | __fzfescape | read -l selects
+    fish -c "$FZF_CTRL_O_COMMAND" | __fzfcmd -m | read -l selects
     and commandline -i "\"$selects\""
     commandline -f repaint
 end
