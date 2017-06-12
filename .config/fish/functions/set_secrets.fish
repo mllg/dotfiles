d3d6c719dfcef90dd9fdc1321798d96890419922
Michel Lang <michellang@gmail.com>
2017-06-12 11:25:39 +0200

    secrets

--- .config/fish/functions/set_secrets.fish
+++ .config/fish/functions/set_secrets.fish
@@ -1,5 +1,6 @@
 function set_secrets
     set -xU OPENMLAPIKEY (pass show openml.api.key)
     set -xU GITHUB_TOKEN (pass show github.token)
+    set -xU GITHUB_PAT (pass show github.token)
     set -xU HOMEBREW_GITHUB_API_TOKEN (pass show github.token)
 end
