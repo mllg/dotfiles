bde6dab6cd90bfb66a10c8b2d1397f049bdbe584
Michel Lang <michellang@gmail.com>
2019-04-08 09:56:13 +0200

    ...

--- .R/rebuild-sys.R
+++ .R/rebuild-sys.R
@@ -1,16 +1,17 @@
+#!/usr/bin/env Rscript
+
 libs = c("libopenblas", "libicu")
 
 packages = character()
 patterns = sprintf("^%s.* => not found$", libs)
-for (lib in .libPaths()[1L]) {
+for (lib in .libPaths()) {
   files = list.files(path = lib, pattern = "\\.so(\\.[0-9]+)*$", recursive = TRUE, 
     ignore.case = TRUE, full.names = TRUE, no.. = TRUE)
   for (file in files) {
     lines = suppressWarnings(trimws(system2("ldd", file, stdout = TRUE, stderr = TRUE)))
     for (pattern in patterns) {
       if (any(grepl(pattern, lines))) {
-        parts = path_split(file)[[1L]]
-        packages = c(packages, parts[length(parts) - 2L])
+        packages = c(packages, basename(dirname(dirname(file))))
       }
     }
   }
