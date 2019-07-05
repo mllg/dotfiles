aa6ef839d99bcf75686432aa2385bfb47e51ded0
Michel Lang <michellang@gmail.com>
2019-07-06 00:36:41 +0200

    ...

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -40,6 +40,7 @@ set -gx MANPAGER "nvim -c 'set ft=man' -"
 # set -gx OPENBLAS_NUM_THREADS 1
 set -gx WEKA_HOME $HOME/.wekafiles
 set -gx COLORTERM 1
+set -gx GTK_USE_PORTAL 1
 set -gx FZF_TMUX 1
 set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git --ignore-file .ignore'
 set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
