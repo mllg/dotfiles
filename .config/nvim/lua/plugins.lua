61736be7bd49bd606535d342b5f27479dc4fcb59
Michel Lang <michellang@gmail.com>
2021-10-17 13:09:46 +0200

    ...

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -125,15 +125,7 @@ require('packer').startup(function()
         end
     }
 
-    use { 'marko-cerovac/material.nvim',
-
-        config = function()
-            vim.g.material_style = 'lighter'
-        end
-    }
-
-    use { "lukas-reineke/indent-blankline.nvim"
-    }
+    use { "lukas-reineke/indent-blankline.nvim" }
 
     use { 'romgrk/barbar.nvim', -- tabline
         requires = { 'kyazdani42/nvim-web-devicons' },
