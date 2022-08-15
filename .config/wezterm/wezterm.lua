3e23094868d96db2014cf9e241a6bbdd3e111627
Michel Lang <michellang@gmail.com>
2022-08-15 10:19:29 +0200

    ...

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+
+return {
+    color_scheme = 'Gruvbox Dark',
+    -- font = wezterm.font('FiraCode Nerd Font Mono'),
+    font_size = 14.0,
+    initial_rows = 999,
+    initial_cols = 999,
+}
