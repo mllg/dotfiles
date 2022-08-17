02e20d67dfe6ebb86b98d420419036e78d911abb
Michel Lang <michellang@gmail.com>
2022-08-17 10:30:48 +0200

    ...

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,18 @@
 local wezterm = require 'wezterm'
+local hostname = wezterm.hostname()
+local font_size
+
+if string.match(hostname, "MacBook") then
+    font_size = 12
+elseif string.match(hostname, "ithaka") then
+    font_size = 14
+else
+    font_size = 12
+end
 
 return {
     color_scheme = 'Gruvbox Dark',
-    -- font = wezterm.font('FiraCode Nerd Font Mono'),
-    -- font_size = 12.0,
+    font_size = font_size,
     initial_rows = 999,
     initial_cols = 999,
 }
