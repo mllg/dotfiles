9d02a26d6b90e01c636576dde77337c38b0f8b61
Michel Lang <michellang@gmail.com>
2022-11-13 21:22:14 +0100

    ...

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -257,15 +257,6 @@ return require('packer').startup(function(use)
         end
     }
 
-    -- git conflict merge helper
-    use {
-        'akinsho/git-conflict.nvim',
-        tag = "*",
-        config = function()
-            require('git-conflict').setup()
-        end
-    }
-
 
     -- EditorConfig Support
     use {
