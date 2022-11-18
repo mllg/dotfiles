011a23c42b45b9d692b0515c82db45c60721170d
Michel Lang <michellang@gmail.com>
2022-11-18 07:06:58 +0100

    ...

--- .config/nvim/ftplugin/r.lua
+++ .config/nvim/ftplugin/r.lua
@@ -7,6 +7,7 @@ opt.spelllang = 'en'
 
 local iron = require('iron.core')
 
+
 local function iron_send(str)
     iron.send('r', { str })
 end
@@ -50,6 +51,35 @@ vim.api.nvim_create_user_command('RHelp',
 )
 
 local map = vim.keymap.set
+
+map('n', '<cr>', function()
+    local pos = vim.api.nvim_win_get_cursor(0)
+    local linenr = pos[1] - 1
+    local buf_line = vim.trim(vim.api.nvim_buf_get_lines(0, linenr, linenr + 1, 0)[1])
+
+    -- check if we are in a roxygen example region
+    -- if yes, remove the "#'" prefix
+    -- this should be done using treesitter :/
+    if vim.startswith(buf_line, "#'") then
+        for i = linenr, 0, -1 do
+            local cur_line = vim.trim(vim.api.nvim_buf_get_lines(0, i, i + 1, 0)[1])
+            if not vim.startswith(cur_line, "#'") then
+                break
+            end
+            if vim.startswith(cur_line, "#' @examples") then
+                buf_line = string.gsub(buf_line, "^#'%s*", "")
+                break
+            end
+        end
+    end
+
+    iron_send(buf_line)
+
+    if (pos[1] < vim.fn.line('$')) then
+        vim.api.nvim_win_set_cursor(0, { pos[1] + 1, pos[2] })
+    end
+end)
+
 map('n', '<leader>rh',
     function()
         iron.send('r', { 'help("' .. vim.fn.expand('<cword>') .. '")' })
