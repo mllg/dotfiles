b265090069e88406a9ea498b4ca82d76654a4750
Michel Lang <michellang@gmail.com>
2022-12-18 15:49:07 +0100

    ...

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -4,52 +4,31 @@ local opt = vim.opt
 opt.termguicolors = true
 opt.cursorline = true
 opt.number = true
-opt.relativenumber = false
 opt.scrolloff = 2
 opt.laststatus = 3
-opt.guifont='FiraCode Nerd Font Mono:h15'
 
 
--- buffers
-opt.autowriteall = true
-opt.autoread = true
-
-
--- formating
-opt.expandtab = true
-opt.tabstop = 4
-opt.shiftwidth = 4
-opt.softtabstop = 4
--- opt.formatoptions:remove({'r', 'c'})
-
-
-opt.wrap = true
+-- -- formating
+-- -- opt.formatoptions:remove({'r', 'c'})
 opt.breakindent = true
 
-
--- searches
-opt.showmatch = true
+-- -- searches
 opt.ignorecase = true
 opt.smartcase = true
-opt.gdefault = true
-opt.wrapscan = true
-opt.inccommand = 'nosplit'
+opt.showmatch = true
+ opt.gdefault = true
 
+-- -- completion
+opt.completeopt = 'menuone,noselect'
 
--- completion
-opt.completeopt = 'menu,menuone,noselect'
--- opt.shortmess:append { c = true }
-
-
--- os integration
+-- -- os integration
 opt.clipboard = 'unnamedplus'
 
-
--- file handling
+-- -- file handling
 opt.undofile = true
 
 
--- localization
+-- -- localization
 opt.spelllang = 'en,de'
 opt.spellsuggest = 'fast,20'
 opt.printoptions = 'paper:A4,number:y'
