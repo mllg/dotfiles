3be9d3e472a3fe5bef79da1d8cca3b52008dd1e5
Michel Lang <michellang@gmail.com>
2023-01-02 16:51:37 +0100

    ...

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -4,17 +4,14 @@ local M = {
     -- ft = { 'r', 'rmd', 'lua' },
 
     dependencies = {
+        'williamboman/mason-lspconfig.nvim',
+        'williamboman/mason.nvim',
         'j-hui/fidget.nvim',
+        'folke/neodev.nvim',
     },
 
     config = function()
         local on_attach = function(_, bufnr)
-            -- NOTE: Remember that lua is a real programming language, and as such it is possible
-            -- to define small helper and utility functions so you don't have to repeat yourself
-            -- many times.
-            --
-            -- In this case, we create a function that lets us more easily define mappings specific
-            -- for LSP related items. It sets the mode, buffer and description for us each time.
             local nmap = function(keys, func, desc)
                 if desc then
                     desc = 'LSP: ' .. desc
@@ -28,9 +25,9 @@ local M = {
             -- nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
             nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+            -- nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
             nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
             nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-            -- nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
             -- nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
             -- nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
@@ -42,60 +39,50 @@ local M = {
             -- nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
             -- nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
             -- nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-            -- nmap('<leader>wl', function()
-            --     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-            -- end, '[W]orkspace [L]ist Folders')
+            -- nmap('<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, '[W]orkspace [L]ist Folders')
 
             -- Create a command `:Format` local to the LSP buffer
-            vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-                if vim.lsp.buf.format then
-                    vim.lsp.buf.format()
-                elseif vim.lsp.buf.formatting then
-                    vim.lsp.buf.formatting()
-                end
-            end, { desc = 'Format current buffer with LSP' })
+            vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_) vim.lsp.buf.format() end, { desc = 'Format current buffer with LSP' })
         end
 
-        local servers = { 'sumneko_lua', 'r_language_server', 'ltex' }
+        local servers = {
+            sumneko_lua = {
+                Lua = {
+                    workspace = { checkThirdParty = false },
+                    telemetry = { enable = false },
+                },
+            },
+            r_language_server = {},
+            ltex = {},
+        }
+
+        -- Setup neovim lua configuration
+        require('neodev').setup()
 
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-        for _, lsp in ipairs(servers) do
-            require('lspconfig')[lsp].setup {
-                on_attach = on_attach,
-                capabilities = capabilities,
-            }
-        end
+        -- Setup mason so it can manage external tooling
+        require('mason').setup()
 
-        -- Make runtime files discoverable to the server
-        local runtime_path = vim.split(package.path, ';')
-        table.insert(runtime_path, 'lua/?.lua')
-        table.insert(runtime_path, 'lua/?/init.lua')
+        -- Ensure the servers above are installed
+        local mason_lspconfig = require 'mason-lspconfig'
 
-        require('lspconfig').sumneko_lua.setup {
-            on_attach = on_attach,
-            capabilities = capabilities,
-            settings = {
-                Lua = {
-                    runtime = {
-                        -- Tell the language server which version of Lua you're using (most likely LuaJIT)
-                        version = 'LuaJIT',
-                        -- Setup your lua path
-                        path = runtime_path,
-                    },
-                    diagnostics = {
-                        globals = { 'vim' },
-                    },
-                    workspace = { library = vim.api.nvim_get_runtime_file('', true) },
-                    -- Do not send telemetry data containing a randomized but unique identifier
-                    telemetry = { enable = false },
-                },
-            },
+        mason_lspconfig.setup {
+            ensure_installed = vim.tbl_keys(servers),
         }
 
-        require('lspconfig').r_language_server.setup {}
+        mason_lspconfig.setup_handlers {
+            function(server_name)
+                require('lspconfig')[server_name].setup {
+                    capabilities = capabilities,
+                    on_attach = on_attach,
+                    settings = servers[server_name],
+                }
+            end,
+        }
 
+        -- Turn on lsp status information
         require('fidget').setup()
     end
 }
