d2c0e9d6711f17a9907a37466fe24eea281378e0
Michel Lang <michellang@gmail.com>
2023-01-06 10:15:59 +0100

    ...

---
+++ .config/nvim/lua/plugins/project.lua
@@ -0,0 +1,13 @@
+local M = {
+    'ahmedkhalf/project.nvim',
+    config = function()
+        require('project_nvim').setup {
+            detection_methods = { 'pattern' },
+            patterns = { '.projectroot', '.git', '.svn', 'Makefile', 'DESCRIPTION' },
+            scope_chdir = 'tab'
+        }
+    end
+
+}
+
+return M
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,7 +5,8 @@ local M = {
         'nvim-lua/plenary.nvim',
         'kyazdani42/nvim-web-devicons',
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-        'nvim-telescope/telescope-z.nvim',
+        'ahmedkhalf/project.nvim',
+        -- 'nvim-telescope/telescope-z.nvim',
     },
 
     cmd = 'Telescope',
@@ -14,19 +15,19 @@ local M = {
         { '<leader>f', '<cmd>Telescope find_files<cr>', desc = 'Find Files' },
         { '<leader>g', '<cmd>Telescope live_grep<cr>', desc = 'Grep Files' },
         { '<leader>b', '<cmd>Telescope buffers<cr>', desc = 'List Buffers' },
-        { '<leader>m', '<cmd>Telescope oldfiles<cr>', desc = 'MRU Files' },
+        { '<leader>m', '<cmd>Telescope oldfiles<cr>', desc = 'List MRU Files' },
+        { '<leader>z', '<cmd>Telescope projects<cr>', desc = 'List Recent Projects' },
         { '<leader>n', '<cmd>Telescope resume<cr>', desc = 'Resume Telescope' },
         { '<leader>t', '<cmd>Telescope treesitter<cr>', desc = 'Treesitter Symbols' },
         { '<leader>y', '<cmd>Telescope yank_history<cr>', desc = 'Yanks' },
         { '<leader>ls', '<cmd>Telescope lsp_workspace_symbols<cr>', desc = 'LSP Workspace Symbols' },
-        { '<leader>z', '<cmd>Telescope z list<cr>', desc = 'List Recent Directories' },
     },
 
     config = function()
         local telescope = require('telescope')
         telescope.load_extension('fzf')
         telescope.load_extension('yank_history')
-        telescope.load_extension('z')
+        telescope.load_extension('projects')
     end,
 }
 
