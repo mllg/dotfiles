0504bf57b0d443c8312ee25ce909e1c5d07b7a2a
Michel Lang <michellang@gmail.com>
2024-07-12 19:08:15 +0200

    ...

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,12 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
-    -- add tsx and treesitter
     vim.list_extend(opts.ensure_installed, {
+      "c",
+      "cpp",
       "fish",
+      "toml",
+      "yaml",
     })
   end,
 }
