6257e4db6417b8f00ec1f85b34d3609a9e2eb525
Michel Lang <michellang@gmail.com>
2024-07-29 10:08:54 +0200

    ...

--- .config/nvim/lua/plugins/r.lua
+++ .config/nvim/lua/plugins/r.lua
@@ -8,14 +8,14 @@ return {
         default_on_ft()
 
         local function send(str) require('r.send').cmd(str) end
-        local cmd = vim.api.nvim_create_user_command
+        local rcmd = vim.api.nvim_create_user_command
 
         -- Commands for package development
-        cmd('RLoadPackage', function() send('devtools::load_all()') end, { nargs = 0 })
-        cmd('RCheckPackage', function() send('devtools::check()') end, { nargs = 0 })
-        cmd('RTestPackage', function(opts) send('devtools::test(filter = "' .. opts.args .. '")') end, { nargs = '?' })
-        cmd('RDocumentPackage', function() send('devtools::document()') end, { nargs = 0 })
-        cmd('RMake', function() send('devtools::document(); devtools::install()') end, { nargs = 0 })
+        rcmd('RLoadPackage', function() send('devtools::load_all()') end, { nargs = 0 })
+        rcmd('RCheckPackage', function() send('devtools::check()') end, { nargs = 0 })
+        rcmd('RTestPackage', function(opts) send('devtools::test(filter = "' .. opts.args .. '")') end, { nargs = '?' })
+        rcmd('RDocumentPackage', function() send('devtools::document()') end, { nargs = 0 })
+        rcmd('RMake', function() send('devtools::document(); devtools::install()') end, { nargs = 0 })
       end
     }
   end
