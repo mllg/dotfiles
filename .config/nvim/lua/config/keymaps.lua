b637da8b416dd5e45b182c0454909ee6f6c8e41d
Michel Lang <michellang@gmail.com>
2024-08-15 12:10:37 +0200

    ...

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,4 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
+vim.keymap.set("n", "<c-n>", ":cnext<cr>", { desc = "Next error" })
+vim.keymap.set("n", "<c-p>", ":cprev<cr>", { desc = "Previous error" })
