e801e8529f4d260c8fe1b89db34238078f3e2520
Michel Lang <michellang@gmail.com>
2025-02-03 10:26:39 +0100

    ...

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,17 +1,13 @@
 local wezterm = require 'wezterm'
-local hostname = wezterm.hostname()
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Gruvbox Dark'
 config.initial_rows = 50
 config.initial_cols = 150
+config.font_size = 12
 
-if string.match(hostname, "MacBook") then
-    config.font_size = 12
-elseif string.match(hostname, "ithaka") then
+if string.match(wezterm.hostname(), "ithaka") then
     config.font_size = 14
-else
-    config.font_size = 12
 end
 
 return config
